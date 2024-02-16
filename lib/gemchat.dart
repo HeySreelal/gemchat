8e7979028a8f67bb0bfd90b60bd6aa396359fe46
Sreelal TS <heysreelal@gmail.com>
2024-02-16 11:42:39 +0530

    💡 Well documented

--- lib/gemchat.dart
+++ lib/gemchat.dart
@@ -3,12 +3,21 @@ import 'package:gemchat/session.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:televerse/televerse.dart';
 
+/// The Telegram Bot instance
+/// Keep your token saved in your system environment variables
 final bot = Bot<GenAISession>(Platform.environment["BOT_TOKEN"]!);
+
+/// The GenerativeModel instance, which is used to interact with the AI model
+///
+/// Create a new environment variable called `GOOGLE_AI_TOKEN` and set it to your API key generated from the https://aistudio.google.com/app/
 final gem = GenerativeModel(
   model: "gemini-pro",
   apiKey: Platform.environment["GOOGLE_AI_TOKEN"]!,
 );
 
+/// This method keeps track of the `/start` command and and greets the user.
+///
+/// It also initializes the session if it's not already initialized.
 Future<void> starter(Context<GenAISession> ctx) async {
   await ctx.reply(
     "Hello, world! I'm Gem, your personal portal to Google's best AI model, Gemini. I can help you with a lot of things, like generating text, summarizing articles, and much more. Just send me a message and I'll do my best to help you out!",
@@ -16,6 +25,9 @@ Future<void> starter(Context<GenAISession> ctx) async {
   _makeSureSessionIsInitialized(ctx);
 }
 
+/// This method is used to handle text messages sent by the user.
+///
+/// Whenever a user sends a text message, this method is called and it sends the message to the AI model to generate a response.
 Future<void> textMessageHandler(Context<GenAISession> ctx) async {
   final prompt = ctx.message!.text!;
   final content = Content.text(prompt);
@@ -32,12 +44,14 @@ Future<void> textMessageHandler(Context<GenAISession> ctx) async {
   }
 }
 
+/// Internal method to make sure the session is initialized before sending a message.
 void _makeSureSessionIsInitialized(Context<GenAISession> ctx) {
   if (ctx.session.chat == null) {
     ctx.session.setSession(gem.startChat());
   }
 }
 
+/// Internal method to send the content in chunks of 4096 characters.
 Future<void> _sendContent(
   Context<GenAISession> ctx,
   List<String> content,
--- lib/session.dart
+++ lib/session.dart
@@ -1,14 +1,13 @@
 import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:televerse/televerse.dart';
 
+/// The Session keeps track of the user's chat session with the AI model.
 class GenAISession extends Session {
   ChatSession? chat;
 
   GenAISession({this.chat});
 
-  static GenAISession init(int id) {
-    return GenAISession();
-  }
+  GenAISession.init(int id);
 
   void setSession(ChatSession chat) {
     this.chat = chat;
